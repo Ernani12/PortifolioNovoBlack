c884591581f24eaf71732b255586c6a91cf505e8
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2024-12-10 16:01:39 -0300

    aleterando  home

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,21 +14,30 @@ import HeroCamera from '../components/HeroCamera.jsx';
 import { calculateSizes } from '../constants/index.js';
 import { HackerRoom } from '../components/HackerRoom.jsx';
 
+import myImage from '/assets/principal.png'; // Adjust the path as needed
+
+
 const Hero = () => {
   // Use media queries to determine screen size
   const isSmall = useMediaQuery({ maxWidth: 440 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
-
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Adrian <span className="waving-hand">👋</span>
+          Hi, I am Ernani <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+
+        <div className="hero_tag flex flex-col items-center justify-center w-full h-auto text-gray_gradient">
+        <p className="text-center mt-6 text-2xl sm:text-3xl font-bold">
+            Creating software with Java
+          </p>
+          <img src={myImage} alt="Software Icon" className="w-[80%] max-w-[800px] h-auto" />
+         
+        </div>
       </div>
 
       <div className="w-full h-full absolute inset-0">
@@ -38,15 +47,13 @@ const Hero = () => {
             <Leva hidden />
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
 
-            <HeroCamera isMobile={isMobile}>
-              <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
-            </HeroCamera>
+
 
             <group>
               <Target position={sizes.targetPosition} />
               <ReactLogo position={sizes.reactLogoPosition} />
               <Rings position={sizes.ringPosition} />
-              <Cube position={sizes.cubePosition} />
+              
             </group>
 
             <ambientLight intensity={1} />
@@ -55,7 +62,7 @@ const Hero = () => {
         </Canvas>
       </div>
 
-      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+      <div className="absolute bottom-[-50px] left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
